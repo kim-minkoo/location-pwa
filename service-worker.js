2ff2354f6e0dcb4819cee7e43a6954851ed6996c
kim-minkoo <mailsock9@gmail.com>
2026-03-19 01:53:03 +0900

    Fix syntax issues in service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,12 @@ const urlsToCache = [
   "/icon-512.png"
 ];
 
-// 설치 단계: 캐시 생성
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// 요청 가로채기: 캐시 우선, 없으면 네트워크 요청
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -23,7 +21,6 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
-// 캐시 갱신: 오래된 캐시 삭제
 self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
